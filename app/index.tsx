54fbfdae7122f3a25dbc3ca00d13740364e1310a
Cardandom <carlosddominguezc@gmail.com>
2025-09-01 17:50:10 -0500

    Installed and tested nativewind, moved global.css file to the root of the project

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Page() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white text-3xl font-bold">
+        Hola NativeWind 👋
+      </Text>
     </View>
   );
 }
